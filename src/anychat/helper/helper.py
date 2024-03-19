89a1c8d4683978ce1e1071c37e5ecd014c468b1c
Shitanshu Shivam <shivam.shitanshu2017@gmail.com>
2024-03-19 20:02:13 +0530

    modifying the helper to create the temp directory if not exists

--- src/anychat/helper/helper.py
+++ src/anychat/helper/helper.py
@@ -3,7 +3,11 @@ import toml
 
 class Helper:
     def createFile(self, file):
-        fileLocation = f"temp/{file.name}"
+        temp_dir = "temp"
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+
+        fileLocation = f"{temp_dir}/{file.name}"
         with open(fileLocation, "wb+") as fileObject:
             fileObject.write(file.getbuffer())
 
@@ -11,15 +15,6 @@ class Helper:
 
     def deleteFile(self, filePath):
         os.remove(filePath)
-
-    # def set_api_key(self,google_api_key):
-    #     with open(".streamlit/secrets.toml", "r+") as f:
-    #         secrets = toml.load(f)
-    #         secrets["palm_api_key"] = google_api_key
-    #         f.seek(0)
-    #         toml.dump(secrets, f)
-    #         f.truncate()
-    #     return True
 
     def set_api_key(self, api_key_name, api_key_value):
         with open(".streamlit/secrets.toml", "r+") as f:
@@ -29,3 +24,4 @@ class Helper:
             toml.dump(secrets, f)
             f.truncate()
         return True
+
